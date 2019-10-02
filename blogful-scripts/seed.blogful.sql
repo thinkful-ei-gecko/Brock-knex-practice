8823dc3e42651bf6d3a439742025258f787fbdda
Brock Boutwell <brockboutwell@gmail.com>
2019-10-02 14:31:19 -0400

    added testing for shoppinglist and CRUD operations

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Fish tricks', now() - '21 days'::INTERVAL, 'boi i love me some fishticks'),
+    ('woah its a dog', now() - '2 days'::INTERVAL, 'boi i love me some dogs'),
+    ('Cat spat', now() - '10 days'::INTERVAL, 'boi i love me some cats'),
+    ('macbook', now() - '20 days'::INTERVAL, 'boi i love me some mac'),
+    ('pc power', now() - '17 days'::INTERVAL, 'boi i love me some pc power'),
+    ('couch', now() - '15 days'::INTERVAL, 'boi i love me some couch'),
+    ('xbox', now() - '19 days'::INTERVAL, 'boi i love me some xbox'),
+    ('nintendo', now() - '18 days'::INTERVAL, 'boi i love me some nintendo'),
+    ('water bottle', now() - '17 days'::INTERVAL, 'boi i love me some water'),
+    ('spoons', now() - '16 days'::INTERVAL, 'boi i love me some spoons'),
+    ('cups', now() - '15 days'::INTERVAL, 'boi i love me some cups'),
+    ('phone', now() - '14 days'::INTERVAL, 'boi i love me some phone'),
+    ('tray', now() - '13 days'::INTERVAL, 'boi i love me some trays'),
+    ('television', now() - '12 days'::INTERVAL, 'boi i love me some tv'),
+    ('beer', now() - '11 days'::INTERVAL, 'boi i love me some beer'),
+    ('socks', now() - '9 days'::INTERVAL, 'boi i love me some socks'),
+    ('makeup', now() - '8 days'::INTERVAL, 'boi i love me some makeup'),
+    ('blanket', now() - '6 days'::INTERVAL, 'boi i love me some blanket'),
+    ('moon globe', now() - '5 days'::INTERVAL, 'boi i love me some moon globes'),
+    ('remote', now() - '2 days'::INTERVAL, 'boi i love me some remote'),
+    ('clothes', now() - '3 days'::INTERVAL, 'boi i love me some clothes')
+;
